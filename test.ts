1fd8da8f1d00510460b054222d81e557ffe1bed7
--replace <cursodeprogramacion@gmail.com>
2022-07-01 22:07:58 -0300

    commit del ejercicio terminado

--- test.ts
+++ test.ts
@@ -1,4 +1,18 @@
 import test from "ava";
+import { User, Product } from "./index";
 
-test("Testeo la clase User", (t) => {});
-test("Testeo la clase Product y sus métodos", (t) => {});
+test("Testeo la clase User", (t) => {
+	const producto = new Product("celu", 200000);
+	t.is(producto.name, "celu");
+	t.is(producto.price, 200000);
+});
+test("Testeo la clase Product y sus métodos", (t) => {
+	const datosDeusuario = new User("nicolas");
+	const producto = new Product("celu", 200000);
+	datosDeusuario.addProduct(producto);
+	console.log(datosDeusuario.products);
+
+	t.is(datosDeusuario.name, "nicolas");
+	t.is(datosDeusuario.products[0].name, "celu");
+	t.is(datosDeusuario.products[0].price, 200000);
+});
